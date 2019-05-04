87937ca021b282d643b8ca70cd4dc7563a887383
Ian Evans <someclouddude@gmail.com>
2019-05-03 22:21:42 -0500

    updating outputs

--- tgw/outputs.tf
+++ tgw/outputs.tf
@@ -1,11 +1,11 @@
 output "tgw_arn" {
     description = "EC2 Transit Gateway Amazon Resource Name (ARN)"
-    value = ""
+    value = "${aws_ec2_transit_gateway.this.arn}"
 }
 
 output "tgw_id" {
     description = "EC2 Transit Gateway identifier."
-    value = ""
+    value = "${aws_ec2_transit_gateway.this.id}"
 }
 
 output "tgw_owner_id" {
