ab121a11d3c0b93f8b54dbf335c7aaf19e484e03
Ian Evans <someclouddude@gmail.com>
2019-05-04 07:19:36 -0500

    hack hack hack

--- tgw/tgw.tf
+++ tgw/tgw.tf
@@ -1,14 +1,16 @@
 data "aws_caller_identity" "current" {
-  
 }
 
 resource "random_integer" "aws_asn" {
+    count = "${var.amazon_side_asn = "" ? 1 : 0}"
     min = 64512
     max = 65534
 }
 
+amazon_side_asn = "${coalesce(var.amazon_side_asn, random_integer.aws_asn.result)}"
+
 resource "aws_ec2_transit_gateway" "this" {
-  amazon_side_asn                 = "${coalesce(var.amazon_side_asn, random_integer.aws_asn.result)}"
+  amazon_side_asn                 = "${var.amazon_side_asn}"
   auto_accept_shared_attachments  = "${var.auto_accept_shared_attachments}"
   default_route_table_association = "${var.default_route_table_association}"
   default_route_table_propagation = "${var.default_route_table_propagation}"
