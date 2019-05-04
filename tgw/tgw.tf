09c9581b7f927301f9e6d4e4f660e241b955784a
Ian Evans <someclouddude@gmail.com>
2019-05-04 06:56:49 -0500

    added random int and coalesce to module for asn

--- tgw/tgw.tf
+++ tgw/tgw.tf
@@ -2,8 +2,13 @@ data "aws_caller_identity" "current" {
   
 }
 
+resource "random_integer" "aws_asn" {
+    min = 64512
+    max = 65534
+}
+
 resource "aws_ec2_transit_gateway" "this" {
-  amazon_side_asn                 = "${var.amazon_side_asn}"
+  amazon_side_asn                 = "${coalesce(var.amazon_side_asn, random_integer.aws_asn.result)}"
   auto_accept_shared_attachments  = "${var.auto_accept_shared_attachments}"
   default_route_table_association = "${var.default_route_table_association}"
   default_route_table_propagation = "${var.default_route_table_propagation}"
