10a188f076203e659a7479d7599c76e046c0e3b1
Ian Evans <someclouddude@gmail.com>
2019-05-04 11:27:30 -0500

    hmm sts error with dynaic secret?

--- tgw/tgw.tf
+++ tgw/tgw.tf
@@ -1,14 +1,13 @@
-data "aws_caller_identity" "current" {
-}
+#data "aws_caller_identity" "current" {}
 
 locals {
   amazon_side_asn = "${coalesce(var.amazon_side_asn, random_integer.aws_asn.result)}"
 }
 
 resource "random_integer" "aws_asn" {
-#    count = "${var.amazon_side_asn == "" ? 1 : 0}"
-    min = 64512
-    max = 65534
+#  count = "${var.amazon_side_asn == "" ? 1 : 0}"
+  min   = 64512
+  max   = 65534
 }
 
 resource "aws_ec2_transit_gateway" "this" {
