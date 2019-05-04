e630457f9be0e76de82cad1e0737d2be14271c8b
Ian Evans <someclouddude@gmail.com>
2019-05-04 11:29:26 -0500

    remove the output too

--- tgw/outputs.tf
+++ tgw/outputs.tf
@@ -8,10 +8,12 @@ output "tgw_id" {
     value = "${aws_ec2_transit_gateway.this.id}"
 }
 
+/*
 output "tgw_owner_id" {
     description = "Identifier of the AWS account that owns the EC2 Transit Gateway"
     value = "${data.aws_caller_identity.current.account_id}"
 }
+*/
 
 output "tgw_asn" {
     description = "Private Autonomous System Number (ASN) for the Amazon side of a BGP session."
