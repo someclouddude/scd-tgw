7bf81fb6862712be75f35426ed2a59a1aa8c5178
Ian Evans <someclouddude@gmail.com>
2019-05-05 09:12:57 -0500

    syntax

--- tgw-vpc-att/main.tf
+++ tgw-vpc-att/main.tf
@@ -4,7 +4,7 @@ resource "aws_ec2_transit_gateway_vpc_attachment" "this" {
   vpc_id                                          = "${var.vpc_id}"
   dns_support                                     = "${var.dns_support}"
   ipv6_support                                    = "${var.ipv6_support}"
-  tags                                            = "${var.tags)}"
+  tags                                            = "${var.tags}"
   transit_gateway_default_route_table_association = "${var.default_rt_assoc}"
   transit_gateway_default_route_table_propagation = "${var.default_rt_prop}"
 }
